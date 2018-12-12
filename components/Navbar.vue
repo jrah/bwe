04cd1754ae2562b7ddf48061d3c4b04ed5624346
jrah <jjames.home@gmail.com>
2018-12-12 12:31:53 +0000

    Add basic components

---
+++ components/Navbar.vue
@@ -0,0 +1,51 @@
+<template>
+
+  <nav
+    :class="{ isOpen: isOpen }">
+    <span
+      class="cursor-pointer"
+      @click="toggleNav()">
+      <font-awesome-icon
+        icon="bars"
+        size="2x"
+      />
+    </span>
+    <!-- <span @click="closeNav(click)" class="pointer z-4">Close</span> -->
+    <a
+      v-for="(item, index) in site.nav"
+      :v-if="item !== null"
+      :key="`nav-${index}`"
+      :href="item.href"
+      class="hidden">
+      <span class="inline">{{ item.text }}</span>
+    </a>
+  </nav>
+
+</template>
+
+<script>
+import site from '~/content/site.json'
+export default {
+  data() {
+    return {
+      site,
+      isOpen: false
+    }
+  },
+  methods: {
+    closeNav() {},
+    toggleNav() {
+      this.isOpen = !this.isOpen
+      console.log('toggle nav')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import 'assets/scss/mixins';
+
+.block-important {
+  display: block !important;
+}
+</style>
