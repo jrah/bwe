0039e13bff2d8ff8fa9b3b34112e5cdb61e0015a
jrah <jjames.home@gmail.com>
2018-12-14 22:05:00 +0000

    Add more components

--- pages/index.vue
+++ pages/index.vue
@@ -5,8 +5,9 @@
       :component-paragraph="home.splash.text"
       :component-title="home.splash.title"/>
     <app-feature-block
-      :component-heading="home.features.heading"
-      :component-paragraph="home.features.paragraph" 
+      :component-title="home.features.title"
+      :component-loop="home.features.block"/>
+    <app-feature-grid
       :component-title="home.features.title"
       :component-loop="home.features.block"/>
   </div>
@@ -18,12 +19,14 @@ import home from '~/content/home.json'
 import AppMainHeader from '~/components/AppMainHeader.vue'
 import AppSplash from '~/components/AppSplash.vue'
 import AppFeatureBlock from '~/components/AppFeatureBlock.vue'
+import AppFeatureGrid from '~/components/AppFeatureGrid.vue'
 
 export default {
   components: {
     AppMainHeader,
     AppSplash,
-    AppFeatureBlock
+    AppFeatureBlock,
+    AppFeatureGrid
   },
   data() {
     return {
