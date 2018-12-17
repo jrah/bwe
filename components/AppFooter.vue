08c25955a7cd7de8559416509f4a08560a87f2f4
jrah <jjames.home@gmail.com>
2018-12-17 02:51:09 +0000

    Add footer

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,30 +1,59 @@
 <template>
   <footer>
+
     <div class="container p-8">
-      <div class="flex justify-between items-center">
-        <span class="block font-black">© 2018 {{ site.title }} </span>
-        <ul
-          v-if="icons"
-          class="pa-0 ma-0 list flex justify-between">
+      <div class="flex py-5">
+        <ul class="list-reset flex justify-between ns:pr-24 ns:w-4/5">
           <li
-            v-for="(icon, index) in icons"
+            v-for="(item, index) in site.nav"
             :key="index"
             :name=" '' + index "
-            class="inline mr-3">
-            <a
-              :href="icon.href"
-              class="text-grey-darkest hover:text-navy-lighter"/>
+            class="">
+            <h2 class="text-grey-darkest font-black text-lg uppercase mb-2">{{ item.text }}</h2>
+            <ul class="list-reset">
+              <li
+                v-for="(inner, index) in item.content"
+                :key="index"
+                :name=" '' + index ">
+                <a
+                  :href="inner.href"
+                  class="text-grey-darker no-underline hover:text-grey"> {{ inner.text }}</a>
+              </li>
+            </ul>
           </li>
         </ul>
-        <div
-          v-else
-          class="bg-red-lightest border-l-4 border-red text-red-dark p-4"
-          role="alert">
-          <p class="font-bold">No icons</p>
+        <div class="ns:w-1/5">
+          <span class="text-grey-darkest text-lg font-bold">{{ site.title }}</span>
+          <ul class="list-reset flex justify-between">
+            <li
+              v-for="(item, index) in site.contact_information"
+              :key="index"
+              :name=" '' + index ">
+
+              <span>{{ item.postal }}</span>
+              <span>{{ item.phone }}</span>
+
+            </li>
+          </ul>
         </div>
+      </div>
+      <div class="list-reset flex justify-end">
+        <a
+          v-for="(icon, index) in site.social"
+          :key="index"
+          :href="icon.href"
+          :name=" '' + index ">
+          <font-awesome-icon
+            :icon="['fab', icon.type]"
+            :style="{color: 'black'}"
+            class="m-2 hover:blue pointer"
+            size="2x"
+          />
+        </a>
       </div>
     </div>
   </footer>
+
 </template>
 
 <script>
