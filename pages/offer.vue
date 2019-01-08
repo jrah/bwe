ca2e2f48240ba5cd4211c60f67b043c2e63559fc
jrah <jjames.home@gmail.com>
2019-01-08 18:35:44 +0000

    Add only testimonial and header

--- pages/offer.vue
+++ pages/offer.vue
@@ -2,25 +2,8 @@
   <!-- add link to bottom of trusted by -->
   <div>
     <app-main-header/>
-    <app-road-map
-      :component-title="home.features.title"
-      :component-loop="home.features.block"/>
-    <app-splash
-      :component-paragraph="home.splash.text"
-      :component-title="home.splash.title"/>
-    <app-accordion
-      :component-side-active="true"
-      :component-side="home.contact.block"
-      :component-title="home.features.title"
-      :component-loop="home.features.block"/>
     <app-testimonial
       :component-loop="home.testimonial"/>
-    <app-accordion
-      :component-title="home.features.title"
-      :component-loop="home.features.block"/>
-    <app-feature-blog
-      :component-title="home.features.title"
-      :component-loop="home.features.block"/>
   </div>
 </template>
 
@@ -28,24 +11,12 @@
 import home from '~/content/home.json'
 
 import AppMainHeader from '~/components/AppMainHeader.vue'
-import AppSplash from '~/components/AppSplash.vue'
-import AppFeatureBlock from '~/components/AppFeatureBlock.vue'
-import AppFeatureGrid from '~/components/AppFeatureGrid.vue'
 import AppTestimonial from '~/components/AppTestimonial.vue'
-import AppFeatureBlog from '~/components/AppFeatureBlog.vue'
-import AppAccordion from '~/components/AppAccordion.vue'
-import AppRoadMap from '~/components/AppRoadMap.vue'
 
 export default {
   components: {
     AppMainHeader,
-    AppSplash,
-    AppFeatureBlock,
-    AppFeatureGrid,
-    AppTestimonial,
-    AppFeatureBlog,
-    AppAccordion,
-    AppRoadMap
+    AppTestimonial
   },
   data() {
     return {
