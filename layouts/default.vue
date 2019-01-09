a9e0a86c17cb5684d2482c1fca6b20d34aa19f13
jrah <jjames.home@gmail.com>
2019-01-09 04:09:36 +0000

    Add prototype pages

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,35 @@
 <template>
   <div class="site-canvas">
-    <div 
-      id="header" 
+    <div
+      id="header"
       class="relative py-3 flex justify-between items-center container">
       <app-logo/>
       <app-navbar/>
     </div>
     <nuxt/>
     <app-footer/>
+    <div class="sticky pin-b max-w-sm mx-auto z-50">
+      <div class="text-center py-4 lg:px-4 flex">
+        <div
+          class="p-2 rounded bg-indigo-darker items-center text-indigo-lightest leading-none lg:rounded-full flex lg:inline-flex"
+          role="alert">
+          <span class="flex rounded-full bg-indigo uppercase px-2 py-1 text-xs font-bold mr-3">Prototype Pages</span>
+          <div class="div">
+            <nuxt-link
+              class="mx-2 text-left text-white no-underline"
+              to="/">Home</nuxt-link>
+            <span class="inline">/</span>
+            <nuxt-link
+              class="mx-2 text-left text-white no-underline"
+              to="/offer">Offer</nuxt-link>
+            <span class="inline">/</span>
+            <nuxt-link
+              class="mx-2 text-left text-white no-underline"
+              to="/contact">Contact</nuxt-link>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
