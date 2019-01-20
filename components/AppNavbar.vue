72f8ec83a1c06d21169fe296f435c31636761b7b
jrah <jjames.home@gmail.com>
2019-01-20 23:22:03 +0000

    Add initial menu items

--- components/AppNavbar.vue
+++ components/AppNavbar.vue
@@ -1,34 +1,31 @@
 <template>
-  <div>
-    <nav class="flex items-center justify-end">
-      <!-- <span @click="closeNav(click)" class="pointer z-4">Close</span> -->
-      <div
-        :class="{ isOpen: isOpen }"
-        class="">
-        <ul class="list-reset flex justify-between items-center z-20 relative">
-          <li
-            v-for="(item, index) in site.nav"
-            :key="index"
-            :name=" '' + index "
-            class="dib mr-6">
-            <a
-              :href="item.href"
-              class="text-white hover:text-blue no-underline text-lg mb-4">{{ item.text }}</a>
-          </li>
-        </ul>
-      </div>
-      <span
-        :class="{ isOpenToggle: isOpen }"
-        class="cursor-pointer relative z-20"
-        @click="toggleNav()">
-        <font-awesome-icon
-          icon="bars"
-          size="2x"
-          color="white"
-        />
-      </span>
-    </nav>
-  </div>
+  <nav 
+    :class="{ isOpen: isOpen }"
+    class="flex items-center justify-end">
+    <!-- <span @click="closeNav(click)" class="pointer z-4">Close</span> -->
+    <ul
+      class="list-reset flex justify-between items-center z-20 relative">
+      <li
+        v-for="(item, index) in site.nav"
+        :key="index"
+        :name=" '' + index "
+        class="dib mr-6">
+        <a
+          :href="item.href"
+          class="text-white hover:text-blue no-underline text-lg mb-4">{{ item.text }}</a>
+      </li>
+    </ul>
+    <span
+      :class="{ isOpenToggle: isOpen }"
+      class="cursor-pointer relative z-20"
+      @click="toggleNav()">
+      <font-awesome-icon
+        icon="bars"
+        size="2x"
+        color="white"
+      />
+    </span>
+  </nav>
 
 </template>
 
