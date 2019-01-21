0269c498b4c34e8ddea255bc04f967d431c36f9e
jrah <jjames.home@gmail.com>
2019-01-21 23:31:35 +0000

    lint

--- components/AppSwitcher.vue
+++ components/AppSwitcher.vue
@@ -5,19 +5,17 @@
         class="p-2 rounded bg-indigo-darker items-center text-indigo-lightest leading-none lg:rounded-full flex lg:inline-flex"
         role="alert">
         <span class="flex rounded-full bg-indigo uppercase px-2 py-1 text-xs font-bold mr-3">Prototype Pages</span>
-        <div class="div">
-          <nuxt-link
-            class="mx-2 text-left text-white no-underline"
-            to="/">Home</nuxt-link>
-          <span class="inline">/</span>
-          <nuxt-link
-            class="mx-2 text-left text-white no-underline"
-            to="/boardingschools">Boarding Schools</nuxt-link>
-          <span class="inline">/</span>
-          <nuxt-link
-            class="mx-2 text-left text-white no-underline"
-            to="/contact">Contact</nuxt-link>
-        </div>
+        <nuxt-link
+          class="mx-2 text-left text-white no-underline"
+          to="/">Home</nuxt-link>
+        <span class="inline">/</span>
+        <nuxt-link
+          class="mx-2 text-left text-white no-underline"
+          to="/boardingschools">Boarding Schools</nuxt-link>
+        <span class="inline">/</span>
+        <nuxt-link
+          class="mx-2 text-left text-white no-underline"
+          to="/contact">Contact</nuxt-link>
       </div>
     </div>
   </div>
