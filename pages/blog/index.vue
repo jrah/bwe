fafcab5a9e4b621557297a3b0103fa44fa172440
jrah <jjames.home@gmail.com>
2019-02-05 13:21:05 +0000

    Add blog pages

---
+++ pages/blog/index.vue
@@ -0,0 +1,42 @@
+<template lang="html">
+  <div>
+    <app-main-header
+      :component-image="grid.image"
+      :component-button="false"
+      :component-form="false"/>
+    <!-- <app-file-index
+      :component-background-color="'grey-lightest'"
+      :component-title="grid.title"
+      :component-loop="grid.block"/> -->
+    <app-feature-blog
+      :component-title="grid.title"
+      :component-paragraph="'This acts as a dual example. You can click on PDFs using the default view behaviour in Chrome'"
+      :component-loop="grid.block"/>
+  </div>
+</template>
+
+<script>
+import grid from '~/content/indexgrid.json'
+
+import AppMainHeader from '~/components/AppMainHeader.vue'
+import AppIndexGrid from '~/components/AppIndexGrid.vue'
+import AppFileIndex from '~/components/AppFileIndex.vue'
+import AppFeatureBlog from '~/components/AppFeatureBlog.vue'
+
+export default {
+  components: {
+    AppMainHeader,
+    AppIndexGrid,
+    AppFileIndex,
+    AppFeatureBlog
+  },
+  data() {
+    return {
+      grid
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ pages/blog/post.vue
@@ -0,0 +1,31 @@
+<template lang="html">
+  <div>
+    <app-main-header
+      :component-image="sample.image"
+      :component-button="false"
+      :component-form="false"/>
+    <app-blog-content/>
+  </div>
+</template>
+
+<script>
+import sample from '~/content/blog/sample.json'
+
+import AppMainHeader from '~/components/AppMainHeader.vue'
+import AppBlogContent from '~/components/AppBlogContent.vue'
+export default {
+  components: {
+    AppMainHeader,
+    AppBlogContent
+  },
+  data() {
+    return {
+      sample
+      // pageTitle: home.title
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
